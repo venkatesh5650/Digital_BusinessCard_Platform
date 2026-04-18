9a13bac222ff43e953204e9f98e64089067fa3f8
Karthan Venkatesh <karthanvenkateshvenkatesh@gmail.com>
2026-04-18 13:07:39 +0530

    fix: automatic OAuth account linking for Google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -29,6 +29,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+            allowDangerousEmailAccountLinking: true,
           }),
         ]
       : []),
